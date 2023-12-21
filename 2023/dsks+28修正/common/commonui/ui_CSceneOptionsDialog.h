cd5f41a7d268154440a7566c878055c86bad4d09
Data-Structure-Qt <120771613+Data-Structure-Qt@users.noreply.github.com>
2023-12-21 18:49:15 +0800

    Add files via upload

---
+++ 2023/dsks+28修正/common/commonui/ui_CSceneOptionsDialog.h
@@ -0,0 +1,381 @@
+/********************************************************************************
+** Form generated from reading UI file 'CSceneOptionsDialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_CSCENEOPTIONSDIALOG_H
+#define UI_CSCENEOPTIONSDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QVBoxLayout>
+#include <colorbutton.h>
+#include <pathpicker.h>
+#include <spinslider.h>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_CSceneOptionsDialog
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QGroupBox *SceneSection;
+    QFormLayout *formLayout;
+    QLabel *label_4;
+    QSint::ColorButton *BackgroundColor;
+    QGroupBox *GridSection;
+    QFormLayout *formLayout_2;
+    QLabel *label;
+    QHBoxLayout *horizontalLayout;
+    QSpinBox *GridSize;
+    QLabel *label_2;
+    QLabel *label_3;
+    QSint::ColorButton *GridColor;
+    QCheckBox *GridSnap;
+    QCheckBox *GridVisible;
+    QGroupBox *PerformanceSection;
+    QGridLayout *gridLayout;
+    QLabel *label_5;
+    QCheckBox *Antialiasing;
+    QLabel *label_6;
+    QSint::SpinSlider *CacheSlider;
+    QGroupBox *DataSection;
+    QGridLayout *gridLayout_2;
+    QCheckBox *EnableBackups;
+    QLabel *label_7;
+    QSpinBox *BackupPeriod;
+    QLabel *label_8;
+    QGroupBox *ExtraSection;
+    QGridLayout *gridLayout_3;
+    QLabel *label_9;
+    QComboBox *GraphvizDefaultEngine;
+    QLabel *label_10;
+    QSint::PathPicker *GraphvizPath;
+    QPushButton *GraphvizTest;
+    QLabel *label_11;
+    QSpacerItem *verticalSpacer;
+    QFrame *line;
+    QDialogButtonBox *buttonBox;
+
+    void setupUi(QDialog *CSceneOptionsDialog)
+    {
+        if (CSceneOptionsDialog->objectName().isEmpty())
+            CSceneOptionsDialog->setObjectName(QString::fromUtf8("CSceneOptionsDialog"));
+        CSceneOptionsDialog->resize(445, 546);
+        verticalLayout = new QVBoxLayout(CSceneOptionsDialog);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        SceneSection = new QGroupBox(CSceneOptionsDialog);
+        SceneSection->setObjectName(QString::fromUtf8("SceneSection"));
+        SceneSection->setFlat(true);
+        formLayout = new QFormLayout(SceneSection);
+        formLayout->setObjectName(QString::fromUtf8("formLayout"));
+        formLayout->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
+        label_4 = new QLabel(SceneSection);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setMinimumSize(QSize(120, 0));
+
+        formLayout->setWidget(0, QFormLayout::LabelRole, label_4);
+
+        BackgroundColor = new QSint::ColorButton(SceneSection);
+        BackgroundColor->setObjectName(QString::fromUtf8("BackgroundColor"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(BackgroundColor->sizePolicy().hasHeightForWidth());
+        BackgroundColor->setSizePolicy(sizePolicy);
+        BackgroundColor->setMinimumSize(QSize(100, 0));
+        BackgroundColor->setPopupMode(QToolButton::InstantPopup);
+        BackgroundColor->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+        BackgroundColor->setArrowType(Qt::NoArrow);
+
+        formLayout->setWidget(0, QFormLayout::FieldRole, BackgroundColor);
+
+
+        verticalLayout->addWidget(SceneSection);
+
+        GridSection = new QGroupBox(CSceneOptionsDialog);
+        GridSection->setObjectName(QString::fromUtf8("GridSection"));
+        GridSection->setFlat(true);
+        formLayout_2 = new QFormLayout(GridSection);
+        formLayout_2->setObjectName(QString::fromUtf8("formLayout_2"));
+        formLayout_2->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
+        label = new QLabel(GridSection);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setMinimumSize(QSize(120, 0));
+
+        formLayout_2->setWidget(0, QFormLayout::LabelRole, label);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        GridSize = new QSpinBox(GridSection);
+        GridSize->setObjectName(QString::fromUtf8("GridSize"));
+        sizePolicy.setHeightForWidth(GridSize->sizePolicy().hasHeightForWidth());
+        GridSize->setSizePolicy(sizePolicy);
+        GridSize->setMinimumSize(QSize(100, 0));
+        GridSize->setMinimum(1);
+        GridSize->setMaximum(40);
+
+        horizontalLayout->addWidget(GridSize);
+
+        label_2 = new QLabel(GridSection);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout->addWidget(label_2);
+
+
+        formLayout_2->setLayout(0, QFormLayout::FieldRole, horizontalLayout);
+
+        label_3 = new QLabel(GridSection);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setMinimumSize(QSize(120, 0));
+
+        formLayout_2->setWidget(2, QFormLayout::LabelRole, label_3);
+
+        GridColor = new QSint::ColorButton(GridSection);
+        GridColor->setObjectName(QString::fromUtf8("GridColor"));
+        sizePolicy.setHeightForWidth(GridColor->sizePolicy().hasHeightForWidth());
+        GridColor->setSizePolicy(sizePolicy);
+        GridColor->setMinimumSize(QSize(100, 0));
+        GridColor->setPopupMode(QToolButton::InstantPopup);
+        GridColor->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+        GridColor->setArrowType(Qt::NoArrow);
+
+        formLayout_2->setWidget(2, QFormLayout::FieldRole, GridColor);
+
+        GridSnap = new QCheckBox(GridSection);
+        GridSnap->setObjectName(QString::fromUtf8("GridSnap"));
+
+        formLayout_2->setWidget(4, QFormLayout::FieldRole, GridSnap);
+
+        GridVisible = new QCheckBox(GridSection);
+        GridVisible->setObjectName(QString::fromUtf8("GridVisible"));
+
+        formLayout_2->setWidget(3, QFormLayout::FieldRole, GridVisible);
+
+
+        verticalLayout->addWidget(GridSection);
+
+        PerformanceSection = new QGroupBox(CSceneOptionsDialog);
+        PerformanceSection->setObjectName(QString::fromUtf8("PerformanceSection"));
+        PerformanceSection->setFlat(true);
+        gridLayout = new QGridLayout(PerformanceSection);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        label_5 = new QLabel(PerformanceSection);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setMinimumSize(QSize(120, 0));
+        label_5->setMaximumSize(QSize(100, 16777215));
+
+        gridLayout->addWidget(label_5, 0, 0, 1, 1);
+
+        Antialiasing = new QCheckBox(PerformanceSection);
+        Antialiasing->setObjectName(QString::fromUtf8("Antialiasing"));
+        QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(Antialiasing->sizePolicy().hasHeightForWidth());
+        Antialiasing->setSizePolicy(sizePolicy1);
+
+        gridLayout->addWidget(Antialiasing, 0, 1, 1, 1);
+
+        label_6 = new QLabel(PerformanceSection);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setMinimumSize(QSize(120, 0));
+        label_6->setMaximumSize(QSize(100, 16777215));
+
+        gridLayout->addWidget(label_6, 1, 0, 1, 1);
+
+        CacheSlider = new QSint::SpinSlider(PerformanceSection);
+        CacheSlider->setObjectName(QString::fromUtf8("CacheSlider"));
+        CacheSlider->setMaximum(1000);
+        CacheSlider->setSingleStep(10);
+        CacheSlider->setPageStep(10);
+        CacheSlider->setOrientation(Qt::Horizontal);
+
+        gridLayout->addWidget(CacheSlider, 1, 1, 1, 1);
+
+
+        verticalLayout->addWidget(PerformanceSection);
+
+        DataSection = new QGroupBox(CSceneOptionsDialog);
+        DataSection->setObjectName(QString::fromUtf8("DataSection"));
+        DataSection->setFlat(true);
+        gridLayout_2 = new QGridLayout(DataSection);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        EnableBackups = new QCheckBox(DataSection);
+        EnableBackups->setObjectName(QString::fromUtf8("EnableBackups"));
+        EnableBackups->setChecked(true);
+
+        gridLayout_2->addWidget(EnableBackups, 1, 1, 1, 1);
+
+        label_7 = new QLabel(DataSection);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        QSizePolicy sizePolicy2(QSizePolicy::Maximum, QSizePolicy::Preferred);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(label_7->sizePolicy().hasHeightForWidth());
+        label_7->setSizePolicy(sizePolicy2);
+
+        gridLayout_2->addWidget(label_7, 1, 3, 1, 1);
+
+        BackupPeriod = new QSpinBox(DataSection);
+        BackupPeriod->setObjectName(QString::fromUtf8("BackupPeriod"));
+        BackupPeriod->setMinimum(5);
+        BackupPeriod->setMaximum(200);
+        BackupPeriod->setSingleStep(5);
+
+        gridLayout_2->addWidget(BackupPeriod, 1, 2, 1, 1);
+
+        label_8 = new QLabel(DataSection);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+        label_8->setMinimumSize(QSize(120, 0));
+        label_8->setMaximumSize(QSize(100, 16777215));
+
+        gridLayout_2->addWidget(label_8, 1, 0, 1, 1);
+
+
+        verticalLayout->addWidget(DataSection);
+
+        ExtraSection = new QGroupBox(CSceneOptionsDialog);
+        ExtraSection->setObjectName(QString::fromUtf8("ExtraSection"));
+        ExtraSection->setFlat(true);
+        gridLayout_3 = new QGridLayout(ExtraSection);
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        label_9 = new QLabel(ExtraSection);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setMinimumSize(QSize(120, 0));
+
+        gridLayout_3->addWidget(label_9, 0, 0, 1, 1);
+
+        GraphvizDefaultEngine = new QComboBox(ExtraSection);
+        GraphvizDefaultEngine->addItem(QString());
+        GraphvizDefaultEngine->addItem(QString());
+        GraphvizDefaultEngine->addItem(QString());
+        GraphvizDefaultEngine->addItem(QString());
+        GraphvizDefaultEngine->addItem(QString());
+        GraphvizDefaultEngine->addItem(QString());
+        GraphvizDefaultEngine->setObjectName(QString::fromUtf8("GraphvizDefaultEngine"));
+        GraphvizDefaultEngine->setEditable(false);
+
+        gridLayout_3->addWidget(GraphvizDefaultEngine, 2, 1, 1, 1);
+
+        label_10 = new QLabel(ExtraSection);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setMinimumSize(QSize(120, 0));
+
+        gridLayout_3->addWidget(label_10, 2, 0, 1, 1);
+
+        GraphvizPath = new QSint::PathPicker(ExtraSection);
+        GraphvizPath->setObjectName(QString::fromUtf8("GraphvizPath"));
+
+        gridLayout_3->addWidget(GraphvizPath, 0, 1, 1, 2);
+
+        GraphvizTest = new QPushButton(ExtraSection);
+        GraphvizTest->setObjectName(QString::fromUtf8("GraphvizTest"));
+
+        gridLayout_3->addWidget(GraphvizTest, 1, 1, 1, 1);
+
+        label_11 = new QLabel(ExtraSection);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+        label_11->setMinimumSize(QSize(120, 0));
+
+        gridLayout_3->addWidget(label_11, 1, 0, 1, 1);
+
+
+        verticalLayout->addWidget(ExtraSection);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        line = new QFrame(CSceneOptionsDialog);
+        line->setObjectName(QString::fromUtf8("line"));
+        line->setFrameShape(QFrame::HLine);
+        line->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout->addWidget(line);
+
+        buttonBox = new QDialogButtonBox(CSceneOptionsDialog);
+        buttonBox->setObjectName(QString::fromUtf8("buttonBox"));
+        buttonBox->setOrientation(Qt::Horizontal);
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+
+        verticalLayout->addWidget(buttonBox);
+
+        QWidget::setTabOrder(BackgroundColor, GridSize);
+        QWidget::setTabOrder(GridSize, GridColor);
+        QWidget::setTabOrder(GridColor, GridVisible);
+        QWidget::setTabOrder(GridVisible, GridSnap);
+        QWidget::setTabOrder(GridSnap, Antialiasing);
+        QWidget::setTabOrder(Antialiasing, CacheSlider);
+        QWidget::setTabOrder(CacheSlider, EnableBackups);
+        QWidget::setTabOrder(EnableBackups, BackupPeriod);
+        QWidget::setTabOrder(BackupPeriod, GraphvizPath);
+        QWidget::setTabOrder(GraphvizPath, GraphvizDefaultEngine);
+
+        retranslateUi(CSceneOptionsDialog);
+        QObject::connect(buttonBox, SIGNAL(accepted()), CSceneOptionsDialog, SLOT(accept()));
+        QObject::connect(buttonBox, SIGNAL(rejected()), CSceneOptionsDialog, SLOT(reject()));
+
+        QMetaObject::connectSlotsByName(CSceneOptionsDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *CSceneOptionsDialog)
+    {
+        CSceneOptionsDialog->setWindowTitle(QCoreApplication::translate("CSceneOptionsDialog", "Scene Options", nullptr));
+        SceneSection->setTitle(QCoreApplication::translate("CSceneOptionsDialog", "\345\234\272\346\231\257", nullptr));
+        label_4->setText(QCoreApplication::translate("CSceneOptionsDialog", "\350\203\214\346\231\257\351\242\234\350\211\262", nullptr));
+        BackgroundColor->setText(QCoreApplication::translate("CSceneOptionsDialog", "...", nullptr));
+        GridSection->setTitle(QCoreApplication::translate("CSceneOptionsDialog", "\347\275\221\346\240\274", nullptr));
+        label->setText(QCoreApplication::translate("CSceneOptionsDialog", "\345\244\247\345\260\217", nullptr));
+        label_2->setText(QCoreApplication::translate("CSceneOptionsDialog", "px", nullptr));
+        label_3->setText(QCoreApplication::translate("CSceneOptionsDialog", "\351\242\234\350\211\262", nullptr));
+        GridColor->setText(QCoreApplication::translate("CSceneOptionsDialog", "...", nullptr));
+        GridSnap->setText(QCoreApplication::translate("CSceneOptionsDialog", "\345\257\271\351\275\220\347\275\221\346\240\274", nullptr));
+        GridVisible->setText(QCoreApplication::translate("CSceneOptionsDialog", "\346\230\276\347\244\272\347\275\221\346\240\274", nullptr));
+        PerformanceSection->setTitle(QCoreApplication::translate("CSceneOptionsDialog", "Performance", nullptr));
+        label_5->setText(QCoreApplication::translate("CSceneOptionsDialog", "Painting", nullptr));
+        Antialiasing->setText(QCoreApplication::translate("CSceneOptionsDialog", "Enable antialiasing", nullptr));
+        label_6->setText(QCoreApplication::translate("CSceneOptionsDialog", "Cache", nullptr));
+        DataSection->setTitle(QCoreApplication::translate("CSceneOptionsDialog", "\346\225\260\346\215\256\345\256\211\345\205\250", nullptr));
+        EnableBackups->setText(QCoreApplication::translate("CSceneOptionsDialog", "\345\244\207\344\273\275\346\225\260\346\215\256\345\234\250\346\257\217", nullptr));
+        label_7->setText(QCoreApplication::translate("CSceneOptionsDialog", "\345\210\206\351\222\237", nullptr));
+        label_8->setText(QString());
+        ExtraSection->setTitle(QCoreApplication::translate("CSceneOptionsDialog", "Extra tools", nullptr));
+        label_9->setText(QCoreApplication::translate("CSceneOptionsDialog", "Path to GraphViz binaries", nullptr));
+        GraphvizDefaultEngine->setItemText(0, QCoreApplication::translate("CSceneOptionsDialog", "dot", nullptr));
+        GraphvizDefaultEngine->setItemText(1, QCoreApplication::translate("CSceneOptionsDialog", "neato", nullptr));
+        GraphvizDefaultEngine->setItemText(2, QCoreApplication::translate("CSceneOptionsDialog", "fdp", nullptr));
+        GraphvizDefaultEngine->setItemText(3, QCoreApplication::translate("CSceneOptionsDialog", "sfdp", nullptr));
+        GraphvizDefaultEngine->setItemText(4, QCoreApplication::translate("CSceneOptionsDialog", "twopi", nullptr));
+        GraphvizDefaultEngine->setItemText(5, QCoreApplication::translate("CSceneOptionsDialog", "circo", nullptr));
+
+        label_10->setText(QCoreApplication::translate("CSceneOptionsDialog", "Default layout engine", nullptr));
+        GraphvizTest->setText(QCoreApplication::translate("CSceneOptionsDialog", "Run dot...", nullptr));
+        label_11->setText(QCoreApplication::translate("CSceneOptionsDialog", "Test GraphViz execution", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class CSceneOptionsDialog: public Ui_CSceneOptionsDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_CSCENEOPTIONSDIALOG_H
---
+++ 2023/dsks+28修正/common/commonui/ui_painterDemo.h
@@ -0,0 +1,110 @@
+/********************************************************************************
+** Form generated from reading UI file 'painterDemo.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_PAINTERDEMO_H
+#define UI_PAINTERDEMO_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_painterDemo
+{
+public:
+    QHBoxLayout *horizontalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *btn1;
+    QPushButton *btn2;
+    QPushButton *btn3;
+    QSpinBox *spinBox;
+    QPushButton *btn;
+    QSpacerItem *verticalSpacer;
+
+    void setupUi(QWidget *painterDemo)
+    {
+        if (painterDemo->objectName().isEmpty())
+            painterDemo->setObjectName(QString::fromUtf8("painterDemo"));
+        painterDemo->resize(563, 125);
+        horizontalLayout_2 = new QHBoxLayout(painterDemo);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        btn1 = new QPushButton(painterDemo);
+        btn1->setObjectName(QString::fromUtf8("btn1"));
+
+        horizontalLayout->addWidget(btn1);
+
+        btn2 = new QPushButton(painterDemo);
+        btn2->setObjectName(QString::fromUtf8("btn2"));
+
+        horizontalLayout->addWidget(btn2);
+
+        btn3 = new QPushButton(painterDemo);
+        btn3->setObjectName(QString::fromUtf8("btn3"));
+
+        horizontalLayout->addWidget(btn3);
+
+        spinBox = new QSpinBox(painterDemo);
+        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+        spinBox->setMinimum(5);
+
+        horizontalLayout->addWidget(spinBox);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        btn = new QPushButton(painterDemo);
+        btn->setObjectName(QString::fromUtf8("btn"));
+
+        verticalLayout->addWidget(btn);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+
+        horizontalLayout_2->addLayout(verticalLayout);
+
+
+        retranslateUi(painterDemo);
+        QObject::connect(btn1, SIGNAL(clicked()), painterDemo, SLOT(showMes()));
+        QObject::connect(btn2, SIGNAL(clicked()), painterDemo, SLOT(say_mes()));
+        QObject::connect(btn3, SIGNAL(clicked()), painterDemo, SLOT(chg_pen()));
+        QObject::connect(btn, SIGNAL(clicked()), painterDemo, SLOT(cleanPix()));
+
+        QMetaObject::connectSlotsByName(painterDemo);
+    } // setupUi
+
+    void retranslateUi(QWidget *painterDemo)
+    {
+        painterDemo->setWindowTitle(QCoreApplication::translate("painterDemo", "Form", nullptr));
+        btn1->setText(QCoreApplication::translate("painterDemo", "\346\233\264\346\224\271\347\224\273\347\254\224\351\242\234\350\211\262", nullptr));
+        btn2->setText(QCoreApplication::translate("painterDemo", "\345\205\263\351\227\255\347\224\273\346\235\277", nullptr));
+        btn3->setText(QCoreApplication::translate("painterDemo", "\346\224\271\345\217\230\347\262\227\347\273\206", nullptr));
+        btn->setText(QCoreApplication::translate("painterDemo", "\346\270\205\347\251\272", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class painterDemo: public Ui_painterDemo {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_PAINTERDEMO_H
