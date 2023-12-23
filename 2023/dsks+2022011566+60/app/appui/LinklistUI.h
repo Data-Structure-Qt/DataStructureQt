e61e36200ce26568b9b1ca3bc7d288fa2ea97423
Data-Structure-Qt <120771613+Data-Structure-Qt@users.noreply.github.com>
2023-12-23 20:04:59 +0800

    三路归并排序

---
+++ 2023/dsks+2022011566+60/app/appui/LinklistUI.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "CNodeEditorScene.h" 
+#include "CQuickHelpUI.h" 
+#include <QWidget> 
+#include "LinklistNode.h" 
+#include<vector>
+class CEditorScene;
+namespace Ui {
+	 class LinklistUI;
+}
+typedef struct Animation { //动画片段
+	int x; // 位置
+	int data; // 数据
+	int flag;
+	int codeid;
+};
+class LinklistUI : public QWidget
+{
+	Q_OBJECT
+public:
+	LinklistUI(QWidget* parent = 0);//构造函数
+	~LinklistUI();//析构函数
+	void setScene(CNodeEditorScene* scene); //传递绘制窗口指针
+	void setCQuickHelpUI(CQuickHelpUI* scene); //传递代码追踪窗口指针
+	void goToBeginButton();
+	void stepBackButton();
+	void playButton();
+	void suspendButton();
+	void stepForwardButton();
+	void goToEndButton();
+	void merge_3(std::vector<int>& arr, int l, int m1, int m2, int r);
+signals:
+	void SearchBtn_SendText();
+private slots:
+	void on_create_btn_clicked(); //创建按钮槽函数
+	void on_clean_btn_clicked(); //清空按钮槽函数
+	void LinklistUI::merge_sort_3(std::vector<int>& arr, int l, int r, int needcen,int nowcen);
+	void LinklistUI::merge_3_sort(std::vector<int>& arr, int l, int r, int needcen,int nowcen);
+	void sleepTime(int time);
+	void animationRecord(int totaltime, int frame, int x, int data);
+	void animationSearchRecord(int totaltime, int frame, int x, int data, int flag, int codeid);
+	void animationClean();
+	void animationPrint(int totaltime, int frametime);
+	void animationDraw(int totaltime, int frametime);
+	void animationDrawSearch(int totaltime, int frametime, int x);
+	void on_three_way_merge_sort();
+protected:
+	void onSceneAttached(CEditorScene* scene);
+	void onSceneDetached(CEditorScene* scene);
+private:
+	Ui::LinklistUI* ui; //控制面板窗口指针
+	CNodeEditorScene* m_scene; //绘制区域窗口指针
+	CNode* m_nodeFactory; //节点工厂类指针
+	CEdge* m_edgeFactory;
+	CQuickHelpUI* m_quickHelp;
+	QList<LinklistNode> QLinkList; //链表节点容器
+	Animation animation[200][200]; //动画 /第几帧/每帧具体步骤
+	Animation animationSearch[200][200]; //动画 /第几帧/每帧具体步骤
+	int frametime[200];
+	int insertIdCode[200];
+	int framesearchtime; // 搜索结点个数
+	int onlytotaltime = 0; // 结点个数
+	int timestamp = 0; //进度控制时间戳
+	int endplaybuttonflag = 0; //停止播放标志位
+	int createflag = 0;
+	int deleteflag = 0;
+	int searchflag = 0;
+};
