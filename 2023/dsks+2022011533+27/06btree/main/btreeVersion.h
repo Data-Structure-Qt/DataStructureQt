090226c72f0c73ee609ebe7f9f4aba51746cbb25
Data-Structure-Qt <120771613+Data-Structure-Qt@users.noreply.github.com>
2023-12-23 16:43:52 +0800

    Delete 2023/dsks+2022011533+27/06btree directory

--- 2023/dsks+2022011533+27/06btree/main/btreeVersion.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef QVGEVERSION_H 
-#define QVGEVERSION_H 
-#include <QVersionNumber> 
-#include <QString> 
-static QString btreeVersionString("0.6.2");
-static QVersionNumber btreeVersion = QVersionNumber::fromString(btreeVersionString);
-
-#endif // QVGEVERSION_H 
