12fdd497456522c41e6921484997019fe252bb20
Data-Structure-Qt <120771613+Data-Structure-Qt@users.noreply.github.com>
2023-12-25 20:06:32 +0800

    No.64

---
+++ 2023/dsks+2022011570+64/app/appmoduels/LinklistNode.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <QPoint>
+#include <QColor>
+class LinklistNode
+{
+public:
+	LinklistNode();//构造函数
+	~LinklistNode();//析构函数
+	QPoint m_Point;//单链表结点坐标位置
+	QColor m_Color; //单链表结点颜色
+	QString m_StrName; //单链表结点名称
+	bool m_Visit;//单链表结点是否访问
+	int m_Row;//链表结点显示行数
+};
